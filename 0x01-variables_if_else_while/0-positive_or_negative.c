366fea87ef6082b4d7887b74d45146d447246a64
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-08-27 06:10:57 -0700

    ftak

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - positive or negative function 
+ * main- positive or negative function. 
  *
- * Return: success
+ * Return: (success)
  */
 int main(void)
 {
