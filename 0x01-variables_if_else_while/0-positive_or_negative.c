5504a2512dac22b374026eb002e30a483afadeb8
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-08-27 05:57:58 -0700

    in the moob

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main- positive or negative function 
+ * main - positive or negative function 
  *
  * Return: success
  */
@@ -11,7 +11,7 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n>0)
+if(n > 0)
 {
 printf("%d is positive\n", n);
 }
