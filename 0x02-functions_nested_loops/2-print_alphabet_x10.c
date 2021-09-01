5679ec24fc0188a2920511b145ddfbd0c0ef14d6
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-09-01 12:45:21 -0700

    good loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet_x10 - print 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+int count, alphabets;
+count = 10;
+while (count > 0)
+{
+for (alphabets = 'a'; alphabets <= 'z', alphabets++)
+{
+_putchar(alphabets);
+}
+_putchar('\n');
+}
+}
