ebc2499ff00402c8f8b748d6d595dad873aabf96
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-09-01 11:31:21 -0700

    main file fixed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
 *
 * main - print _putchar
@@ -8,6 +8,12 @@
 
 int main(void)
 {
-println("_putchar");
+int str[] = {95,112,117,116,99,104,97,114};
+int count, sz;
+sz = sizeof(str) / sizeof(int);
+for(count = 0; count < sz ; count++){
+_putchar(str[count]);
+}
+_putchar('\n');
 return (0);
 }
