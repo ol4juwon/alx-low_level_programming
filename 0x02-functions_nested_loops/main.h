632c9079094ab43d5ce530523fa235c2a86fdd04
olajuwon lawal <olajuwonlawal2012@gmail.com>
2021-09-01 14:14:55 -0700

    sd

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,11 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int ab);
 int print_last_digit(int nld);
+void jack_bauer(void);
+void times_table(void);
+int add(int one, int two);
+
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
 
