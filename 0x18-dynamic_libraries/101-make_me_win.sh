3d39ecfe7552bd4d3107b5a85c3016c2f59f2723
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-04 13:26:04 -0800

    tep lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp/ https://github.com/ol4juwon/alx-low_level_programming/raw/main/0x18-dynamic_libraries/loadhack.so
-export LD_PRELOAD=../loadhack.so
+export LD_PRELOAD=/tmp/loadhack.so
