6376e1919f279f6fcca880b27f667653c8d88c63
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-04 15:19:12 -0800

    fixes

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,5 +13,15 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 if (n == NULL)
 return (-1);
-
+unsigned int count = 0;
+while (n != 0)
+{
+if (count == index)
+{
+if (n % 2)
+return (1);
+else
+return (0);
+}
+}
 }
