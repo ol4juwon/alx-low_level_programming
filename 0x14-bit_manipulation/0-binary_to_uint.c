661e604059297b3488545e9c89b4af4a34a72452
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-04 14:10:30 -0800

    d

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 /*
  * binary_to_uint - converter
  * @binary:  string binary
  * return: uint
  */
 
-unsigned int binary_to_unit(const char *binary){
-	unsigned int base = 0, length = 0, index = 0;
+unsigned int binary_to_uint(const char *binary)
+{
+unsigned int base = 0, length = 0, index = 0;
+if (binary == NULL)
+{
+return (0);
+}
 
-	if(binary == null){
-	return (0);
-	}
-
-	length = strlen(binary);
-	for(; i < length; index++){
-	  if(binary[length] != 48 && binary [length] != 49){
-	 	return (0);
-	  }
-	  if(binary[length] == 49){
-	  i += 1 << base;
-	  }
-	  base++;
-	}
+length = strlen(binary);
+for (; index < length; index++)
+{
+if (binary[length] != 48 && binary[length] != 49)
+{
+return (0);
+}
+if (binary[length] == 49)
+{
+index += 1 << base;
+}
+base++;
+}
 return (index);
 }
