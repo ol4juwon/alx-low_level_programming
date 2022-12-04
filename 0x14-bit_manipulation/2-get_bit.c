725bab46931fc010ea9b3ef46c054ab0ee1d4981
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-04 15:15:16 -0800

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * get_bit - function
+ * @n: int
+ * @index: positon
+ *
+ * Return: int
+ * descrioption: function to
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (n == NULL)
+return (-1);
+
+}
