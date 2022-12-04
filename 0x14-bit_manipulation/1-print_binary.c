caef8113cd97a45f5b209b2a46db9ad8bc445950
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-04 15:08:51 -0800

    fixes

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * converter - function converter
+ * @n: unsigned long int
+ * description: convert
+ * Return: void.
+ */
+void converter(unsigned long int n)
+{
+if (n < 1)
+return;
+converter(n >> 1);
+if (n & 1)
+putchar('1');
+else
+putchar('0');
+}
+
+/**
  * print_binary - function to inary
  * @n: unsigned long int
  *
@@ -17,24 +34,4 @@ putchar('0');
 return;
 }
 converter(n);
-}
-
-/**
- * converter - function converter
- * @n: unsigned long int
- * description: convert
- * Return: void.
- */
-
-void converter(unsigned long int n)
-{
-if (n < 1)
-return;
-
-converter(n >> 1);
-
-if (n & 1)
-	putchar('1');
-else
-	putchar('0');
 }
