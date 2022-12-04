050376e0593ecfc6228ea50b41d196c86dc215ea
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-04 13:23:10 -0800

    gm

--- 0x18-dynamic_libraries/loadhack.c
+++ 0x18-dynamic_libraries/loadhack.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/*
+ * printf - print.
+ * @format: char.
+ * Return: integer.
+ */
+
 int printf(const char *format, ...)
 {
 write(1, "9 8 10 24 75 - 9\n", 17);
 write(1, "Congratulations, you win the Jackpot!\n", 38);
-exit(EXIT_SUCCESS);
+exit(0);
 }
