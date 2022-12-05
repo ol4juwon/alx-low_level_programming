5a90336fd871e883a8cd2ba90b021157ac8c297f
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-05 10:34:44 -0800

    swed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 size_t index;
 if (array == NULL)
 return (-1);
-for (index = 0; index <  size, index++)
+for (index = 0; index <  size; index++)
 {
 if (array[index] == value)
 {
