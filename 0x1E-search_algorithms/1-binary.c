fd3fdfed2b384a88d9c14e1bd026436ca7a2c6e8
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-05 11:28:06 -0800

    search done

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,6 +3,32 @@
 #include "search_algos.h"
 
 /**
+ * search - search each array object
+ */
+
+size_t search(int *array, size_t size, int value)
+{
+size_t mid = size/2;
+size_t x;
+if (!array || size == 0)
+return (-1);
+printf("searching in array: ");
+for (x = 0; x < size; x++)
+printf("%d%s", array[x], (x != size -1) ? ", " : "\n");
+
+if (mid && size % 2 == 0)
+mid--;
+
+if ( array[mid] == value)
+return mid;
+if (array[mid] > value)
+return (search(array, mid, value));
+
+mid++;
+return (search(arrray, mid, value));
+}
+
+/**
  * binary_search - search a sorted array
  * @array:  array pointer to search
  * @size: size of array
@@ -11,7 +37,12 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-size_t index;
+int index;
 if (array == NULL)
 return (-1);
+index = search(array, size, value);
+
+if( index >= 0 && array[index] != value)
+	return (-1);
+return index;
 }
