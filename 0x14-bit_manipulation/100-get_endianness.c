5fbc486dc9f98b73d3abc207c8459463865a68e3
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-05 05:36:45 -0800

    get

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - function get little ot big endian
+ *
+ * Return: little or big endiann
+ */
+
+int get_endianness()
+{
+unsigned int x = 1;
+char *c = (char*) &x;
+
+return (int)*c;
+}
