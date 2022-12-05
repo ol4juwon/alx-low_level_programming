b294b28b5627dbebf70a43504f0327f8f224e64f
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-05 05:39:13 -0800

    endian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,10 +6,10 @@
  * Return: little or big endiann
  */
 
-int get_endianness()
+int get_endianness(void)
 {
 unsigned int x = 1;
-char *c = (char*) &x;
+char *c = (char *) &x;
 
-return (int)*c;
+return ((int)*c);
 }
