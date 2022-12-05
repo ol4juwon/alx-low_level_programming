558ddced29de7afe5b6abc4523b4de7b12c9ca5b
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-05 05:28:45 -0800

    l

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * * flip_bit - Gets the value of a bit at a given index.
+ * * flip_bits - Gets the value of a bit at a given index.
  * @n: The bit.
  * @m: The index to get the value at - indices start at 0.
  *
  * Return: If an error occurs - -1.
  *         Otherwise - The value of bit at index.
  */
-int flip_bit(unsigned long int n, unsigned long int m)
+int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int xor = n ^ m, bits = 0;
 
