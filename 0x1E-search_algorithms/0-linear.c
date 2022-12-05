534df45e5b901e9c15a7ac55a9e1c493abe04fdc
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-05 10:05:47 -0800

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * linear_search - search for a vlaue in an array
+ * @array: point to array to search
+ * @size: size of array 
+ * @value: item to search
+ * Return: -1 if not found or
+ * index if found
+ */
+
+int linear search(int *array, size_t size, int value)
+{
+if (array == NULL)
+return (-1);
+
+}
