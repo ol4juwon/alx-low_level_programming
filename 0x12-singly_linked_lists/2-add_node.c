17a114d9ae18b1d3a4769198aae78ae5f1c1b770
ol4juwon <olajuwonlawal2012@gmail.com>
2022-12-07 08:39:39 +0100

    linter

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,23 +4,24 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
+ * add_node - adds a new node at the beginning of a listint_t list
  * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
+ * @str: string to be included in node
  * Return: address of the new element or NULL if it fails
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
+list_t *new;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
 
-    new->str = strdup(str);
-    new->next = *head;
-    *head = new;
+new->str = strdup(str);
+new->len = strlen(str);
+new->next = *head;
+*head = new;
 
-    return (new);
+return (new);
 }
